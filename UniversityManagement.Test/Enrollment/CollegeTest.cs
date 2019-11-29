4073ff4bff4e88b03d80ff90ef1c377e1e4eede7
Jarrod Swords <jarrod.swords@gmail.com>
2019-11-29 00:57:25 -0500

    Split files

--- UniversityManagement.Test/Enrollment/CollegeTest.cs
+++ UniversityManagement.Test/Enrollment/CollegeTest.cs
@@ -19,17 +19,4 @@ namespace UniversityManagement.Test.Enrollment
             createCollege.Should().Throw<ArgumentException>();
         }
     }
-
-    public class MajorTest
-    {
-        [Theory]
-        [InlineData(0, 1)]
-        [InlineData(1, 0)]
-        public void WhenInstantiating_WithAnInvalidArgument_ThrowArgumentException(long id, long disciplineId)
-        {
-            Action createMajor = () => new Major(id, disciplineId);
-
-            createMajor.Should().Throw<ArgumentException>();
-        }
-    }
 }
---
+++ UniversityManagement.Test/Enrollment/MajorTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FluentAssertions;
+using UniversityManagement.Domain.Write.Enrollment;
+using Xunit;
+
+namespace UniversityManagement.Test.Enrollment
+{
+    public class MajorTest
+    {
+        [Theory]
+        [InlineData(0, 1)]
+        [InlineData(1, 0)]
+        public void WhenInstantiating_WithAnInvalidArgument_ThrowArgumentException(long id, long disciplineId)
+        {
+            Action createMajor = () => new Major(id, disciplineId);
+
+            createMajor.Should().Throw<ArgumentException>();
+        }
+    }
+}
