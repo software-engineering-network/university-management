20d2f7a03efcb405242705c69e4a03947517553f
Jarrod Swords <jswords@igsenergy.com>
2019-12-02 10:34:20 -0500

    Add nested vm example

---
+++ UniversityManagement.Wpf/app/Enrollment/create-application-2/applicant/IApplicantViewModel2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UniversityManagement.Domain.Write;
+
+namespace UniversityManagement.Wpf.Enrollment
+{
+    public interface IApplicantViewModel2
+    {
+        #region Properties
+
+        string Name { get; set; }
+        string NameValidationMessage { get; }
+        string Surname { get; set; }
+        string SurnameValidationMessage { get; }
+        IValidationResult ValidationResult { set; }
+
+        #endregion
+
+        event EventHandler NameChangedHandler;
+        event EventHandler SurnameChangedHandler;
+    }
+}
---
+++ UniversityManagement.Wpf/app/Enrollment/create-application-2/application/ApplicationViewModel2.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using UniversityManagement.Domain.Read.Enrollment;
+using UniversityManagement.Domain.Write;
+using UniversityManagement.Services.Enrollment;
+
+namespace UniversityManagement.Wpf.Enrollment
+{
+    public class ApplicationViewModel2 : ViewModelBase
+    {
+        #region Fields
+
+        private readonly Application _application;
+        private readonly ICreateApplicationService _service;
+
+        private IApplicantViewModel2 _applicant;
+        private IValidationResult _validationResult;
+
+        #endregion
+
+        #region Properties
+
+        public IApplicantViewModel2 Applicant
+        {
+            get => _applicant;
+            set
+            {
+                _applicant = value;
+                _applicant.NameChangedHandler += Validate;
+                _applicant.SurnameChangedHandler += Validate;
+            }
+        }
+
+        private IValidationResult ValidationResult
+        {
+            set
+            {
+                _validationResult = value;
+
+                Applicant.ValidationResult = _validationResult;
+            }
+        }
+
+        #endregion
+
+        #region Construction
+
+        public ApplicationViewModel2(
+            ICreateApplicationService service,
+            Application application = null
+        )
+        {
+            _service = service;
+
+            _application = application == null
+                ? new Application()
+                : application;
+
+            Applicant = CreateApplicantViewModel(_application);
+
+            Validate();
+        }
+
+        #endregion
+
+        private static IApplicantViewModel2 CreateApplicantViewModel(Application application)
+        {
+            if (application.Applicant == null)
+                application.Applicant = new Applicant();
+
+            return new ApplicantViewModel(application.Applicant);
+        }
+
+        private void Validate()
+        {
+            ValidationResult = _service.Validate(_application);
+        }
+
+        private void Validate(object sender, EventArgs args)
+        {
+            Validate();
+        }
+    }
+}
